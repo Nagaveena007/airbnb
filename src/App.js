b174f61a646f4da383b215e0a8ef7d558f9c0ae7
Nagaveena007 <nagaveena.hallikeri@gmail.com>
2022-01-07 16:57:35 +0100

    finished with nav bar search button

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Component } from "react";
-import MyNav from "./components/MyNav/MyNav";
 import Inspiration from "./components/Inspiration/Inspiration";
 import Discover from "./components/Discover/Discover";
 import Home from "./components/Home/Home";
-import Demo from "./components/Home/Demo";
 import FooterPart from "./components/FooterPart/FooterPart";
-import Nav1 from "./components/MyNav/Nav";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/MyNav/Navbar";
+import Navbar1 from "./components/MyNav/Navbar";
 function App() {
   return (
     <>
-      {/*    <MyNav /> */}
-      {/*     <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact />
-        </Switch>
-      </Router> */}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          <Route path="/" element={<Navbar1 />} />
         </Routes>
       </BrowserRouter>
-      {/*  <Nav1 /> */}
-
       <Home />
       <Inspiration />
       <Discover />
--- src/components/MyNav/Navbar.js
+++ src/components/MyNav/Navbar.js
@@ -1,91 +1,201 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Button from "./Button";
 import "./Navbar.css";
-import { MdFingerprint } from "react-icons/md";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { IconContext } from "react-icons/lib";
+import {
+  Nav,
+  Navbar,
+  Col,
+  Row,
+  Container,
+  Image,
+  Button,
+} from "react-bootstrap";
+import { FiGlobe } from "react-icons/fi";
+import { IoPersonCircleSharp } from "react-icons/io5";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiSearch } from "react-icons/fi";
 
-function Navbar() {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+const Navbar1 = () => {
+  let src;
+  const navElements = [
+    {
+      name: "Place to stay",
+    },
+    {
+      name: "Experiences",
+    },
+    {
+      name: "Online Experiences",
+    },
+  ];
+  const [navbar, setNavbar] = useState(false);
+  const [logo, setLogo] = useState(false);
+  const [globe__btn, setGlobe] = useState(false);
+  const [become__host__btn, setHost] = useState(false);
+  const [profile__btn, setUser] = useState(false);
+  const [isScroll, setIsscroll] = useState(false);
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setNavbar(true);
     } else {
-      setButton(true);
+      setNavbar(false);
     }
   };
+  const changeLogo = () => {
+    if (window.scrollY >= 80) {
+      setLogo(true);
+      src =
+        "https://1000logos.net/wp-content/uploads/2017/08/Airbnb-symbol.jpg";
+    } else {
+      setLogo(false);
+      src =
+        "https://logosmarken.com/wp-content/uploads/2020/07/Airbnb-Logo.png";
+    }
+  };
+  const changeGlobe = () => {
+    if (window.scrollY >= 80) {
+      setGlobe(true);
+    } else {
+      setGlobe(false);
+    }
+  };
+  const changeHost = () => {
+    if (window.scrollY >= 80) {
+      setHost(true);
+    } else {
+      setHost(false);
+    }
+  };
+  const changeUser = () => {
+    if (window.scrollY >= 80) {
+      setUser(true);
+    } else {
+      setUser(false);
+    }
+  };
+  const onScroll = () => {
+    if (window.scrollY >= 100) {
+      setIsscroll(true);
+    } else {
+      setIsscroll(false);
+    }
+  };
+  window.addEventListener("scroll", changeBackground);
+  window.addEventListener("scroll", changeLogo);
+  window.addEventListener("scroll", changeGlobe);
+  window.addEventListener("scroll", changeHost);
+  window.addEventListener("scroll", changeUser);
 
-  useEffect(() => {
-    showButton();
-    window.addEventListener("resize", showButton);
-    return window.removeEventListener("resize", showButton);
-  }, []);
-
+  useEffect(() => window.addEventListener("scroll", onScroll), []);
   return (
     <>
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <nav className="navbar">
-          <div className="navbar-container container">
-            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-              <MdFingerprint className="navbar-icon" />
-              LAVISH
-            </Link>
-            <div className="menu-icon" onClick={handleClick}>
-              {click ? <FaTimes /> : <FaBars />}
+      <Navbar
+        bg="bg-dark"
+        variant="dark"
+        expand="lg"
+        className={navbar ? "navbar active" : "navbar"}
+      >
+        <Navbar.Brand className={logo ? "logo active" : "logo"}>
+          <Image />
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="nav__bar__center">
+          {isScroll ? (
+            <div style={{}} className="search__box__div__onScroll">
+              <Container className="search__container__onscroll">
+                <Row className="search__bar__elements__row">
+                  <small>start your search</small>
+                  <Col md={3} className="mt-1 d-flex">
+                    <div className="" style={{ paddingLeft: "4.4em" }}>
+                      <span className="search__btn__onscroll ">
+                        <FiSearch
+                          className=""
+                          style={{ height: "20px", width: "20px" }}
+                        />
+                      </span>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
             </div>
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-              <li className="nav-item">
-                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/services"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
+          ) : (
+            <Nav className="">
+              {navElements.map((e, i) => {
+                return (
+                  <Nav.Link key={i} className="nav-links">
+                    {e.name}
+                  </Nav.Link>
+                );
+              })}
+            </Nav>
+          )}
+        </Navbar.Collapse>
+        <Button
+          className={
+            become__host__btn ? "become__host__btn active" : "become__host__btn"
+          }
+        >
+          Become a Host
+        </Button>
+        <Button className={globe__btn ? "globe__btn active" : "globe__btn"}>
+          <FiGlobe />
+        </Button>
+        <button
+          className={profile__btn ? "profile__btn active" : "profile__btn"}
+        >
+          <AiOutlineMenu className="profile__more__btn" />
+          <IoPersonCircleSharp className="person__btn" />
+        </button>
+      </Navbar>
+      <div style={{}} className="search__box__div">
+        <Container className="search__container">
+          <Row className="search__bar__elements__row">
+            <Col md={3} className="  mt-1  search__inputs">
+              <small className="search__bar__small__text"> Location</small>
+              <br />
+              <span className="search__text"> Where are you goining?</span>
+            </Col>
+            <span className="search__hr__line mr-2"></span>
+            <Col md={2} className="mt-1  search__inputs">
+              <small className="search__bar__small__text">Check in</small>
+              <br />
+              <span className="search__text">Add dates</span>
+            </Col>
+            <span className="search__hr__line mr-2"></span>
+            <Col md={3} className="mt-1  search__inputs">
+              <small className="search__bar__small__text"> Check out</small>
+              <br />
+              <span className="search__text">Add dates</span>
+            </Col>
+            <span className="search__hr__line mr-2"></span>
+            <Col md={3} className="mt-1  search__inputs d-flex">
+              <div className="" role="button">
+                <span>
+                  <small className="search__bar__small__text"> Guests</small>
+                </span>
+
+                <br />
+                <span className="search__text  ">Add guests</span>
+              </div>
+              <div className=" ml-auto">
+                <span
+                  className="search__btn mr-auto mt-2"
+                  style={{ paddingLeft: "3.4em" }}
                 >
-                  Services
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/products"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  Products
-                </Link>
-              </li>
-              <li className="nav-btn">
-                {button ? (
-                  <Link to="/sign-up" className="btn-link">
-                    <Button buttonStyle="btn--outline">SIGN UP</Button>
-                  </Link>
-                ) : (
-                  <Link to="/sign-up" className="btn-link">
-                    <Button
-                      buttonStyle="btn--outline"
-                      buttonSize="btn--mobile"
-                      onClick={closeMobileMenu}
-                    >
-                      SIGN UP
-                    </Button>
-                  </Link>
-                )}
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </IconContext.Provider>
+                  <FiSearch
+                    className=""
+                    style={{ height: "20px", width: "20px" }}
+                  />
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
-}
+};
 
-export default Navbar;
+export default Navbar1;
